ffabae260e0715c95066fcf20e251e18ef1c46bc
admin <admin@DESKTOP-BM97C0L>
2021-03-13 21:13:50 -0500

    VERSION 2.0

--- src/bus/Bus.java
+++ src/bus/Bus.java
@@ -15,7 +15,8 @@ public class Bus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Juan Perez es gay");
+        System.out.println("Juan Perez lo confirmo");
     }
     
 }
